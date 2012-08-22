4b06fd7bf481c710d67d546d1dec2943f106d47f
Nimphious <Nimphious@syntheticsanity.com>
2012-08-22 11:51:07 +1000

    So many changes.

--- test.js
+++ test.js
@@ -7,6 +7,7 @@ var points = [];
 var player = {
 	position: new THREE.Vector3(),
 	speed: 0,
+	thrust: 0,
 	direction: 0,
 	oversteer: 0,
 	geom: null,
@@ -28,6 +29,8 @@ var actx, samples = {}, sounds = [];
 
 var ctrlAcc, ctrlDec, ctrlStr = 0;
 
+var sun;
+
 function init() {
 	renderer = new THREE.WebGLRenderer({antialias:true});
 	renderer.setSize(window.innerWidth, window.innerHeight);
@@ -39,7 +42,10 @@ function init() {
 	camera.lookAt(new THREE.Vector3(0,0,0));
 	scene.add(camera);
 
-	scene.add(new THREE.AmbientLight(0xffffff));
+	//sun = new THREE.AmbientLight(0xffffff);
+	sun = new THREE.DirectionalLight();
+
+	scene.add(sun);
 
 	var loader = new THREE.JSONLoader();
 	var map_diff = THREE.ImageUtils.loadTexture('images/racer_diff.png');
@@ -68,10 +74,12 @@ function init() {
 		uniforms: {
 			'tNoise': { type: 't', value: 0, texture: map_noise },
 			'fHeat': { type: 'f', value: 0 },
-			'fTime': { type: 'f', value: 0 }
+			'fTime': { type: 'f', value: 0 },
+			'mNorm': { type: 'm', value: new THREE.Matrix4() }
 		},
 		transparent: true
 	});
+	mat2.depthWrite = false;
 	loader.load('models/racer.js', function(geom) {
 		var mesh = new THREE.Mesh(geom, mat);
 		player.geom = geom;
@@ -82,6 +90,7 @@ function init() {
 			var mesh = new THREE.Mesh(geom, mat2);
 			player.mesh.add(mesh);
 		});
+		//player.mesh.add(new THREE.Mesh(new THREE.SphereGeometry(5, 128, 64), mat2));
 	});
 
 	points.push(new THREE.Vector3(-5, 0, 50));
@@ -120,9 +129,9 @@ function init() {
 	clock = new THREE.Clock();
 
 	//window.addEventListener('mousemove', function(e) { player.rpm = 1 - e.clientY / window.innerHeight; }, false);
-	window.addEventListener('keydown', function(e) { keys[e.keyCode] = true; }, false);
-	window.addEventListener('keyup', function(e) { keys[e.keyCode] = false; }, false);
-	window.addEventListener('resize', function() { renderer.setSize(window.innerWidth, window.innerHeight); camera.aspect = window.innerWidth / window.innerHeight; camera.updateProjectionMatrix(); }, false);
+	window.addEventListener('keydown', function(e) { keys[e.keyCode] = true; e.preventDefault(); return false; }, false);
+	window.addEventListener('keyup', function(e) { keys[e.keyCode] = false; e.preventDefault(); return false; }, false);
+	window.addEventListener('resize', function() { renderer.setSize(window.innerWidth, window.innerHeight); camera.aspect = window.innerWidth / window.innerHeight; camera.updateProjectionMatrix(); e.preventDefault(); return false; }, false);
 	for (var i = 0; i < 255; i++) keys[i] = false;
 
 	setInterval(loop, 16);
@@ -184,6 +193,7 @@ function loop() {
 
 	ctrlStr += (str - ctrlStr) * 0.1 / (player.speed > 1 ? player.speed : 1);
 
+
 	player.speed = player.speed + (ctrlAcc + -0.3 * (1 + 10 * ctrlDec)) * delta;
 	player.rpm = player.speed / 3;
 	//player.speed -= 0.2 * delta;
@@ -193,33 +203,32 @@ function loop() {
 	player.direction = (player.direction - ctrlStr * delta) % (Math.PI * 2);
 
 	player.position.set(player.position.x - Math.sin(player.direction) * player.speed * 100 * delta, player.position.y, player.position.z - Math.cos(player.direction) * player.speed * 100 * delta);
-	player.mesh.position.set( player.position.x, 0.4 + 0.2 * player.rpm + Math.sin(Date.now() % 2000 / 1000 * Math.PI) * 0.1, player.position.z );
+	player.mesh.position.set( player.position.x, 0.4 + 0.2 * player.rpm + Math.min(1.4, player.speed / 1.4) * (1 - Math.cos(ctrlStr*0.78)) + Math.sin(Date.now() % 2000 / 1000 * Math.PI) * 0.1, player.position.z );
 
 	var m = new THREE.Matrix4();
 	m.rotateByAxis(new THREE.Vector3(1, 0, 0), player.rpm * 0.1);
 	m.rotateByAxis(new THREE.Vector3(0, 1, 0), player.direction);
 	//player.mesh.rotation = m.multiplyVector3();
+	//player.mesh.eulerOrder = 'YXZ';
+	player.mesh.rotation.setX(player.thrust * 0.1);
 	player.mesh.rotation.setY(player.direction - ctrlStr * player.speed * 0.2);
-	player.mesh.rotation.setZ(ctrlStr * -0.25 + ctrlStr * player.speed * -0.1);
+	player.mesh.rotation.setZ(ctrlStr * -Math.min(0.25, player.speed / 8) + ctrlStr * player.speed * -0.1);
 
+	player.mesh.updateMatrixWorld(true);
 
-	camera.position.set(
-		(player.position.x + p.x),
-		(player.position.y + p.y),
-		(player.position.z + p.z)
-	);
-
-	/*camera.position.addSelf(new THREE.Vector3(
+	camera.position.addSelf(new THREE.Vector3(
 		((player.position.x + p.x) - camera.position.x) * delta * 16,
 		((player.position.y + p.y) - camera.position.y) * delta * 16,
 		((player.position.z + p.z) - camera.position.z) * delta * 16
-	));*/
+	));
+
+	//player.mesh.lookAt(camera.position);
 	camera.lookAt(player.position);
 
-	//mat.uniforms.fHeat.value = Math.sin(Date.now() % 2000 / 1000 * Math.PI) * 0.75 + 1 + 0.2 * Math.random();
 	mat.uniforms.fHeat.value = (player.rpm * 2.25 + 0.25) * (1.0 + 0.2 * Math.random());
 	mat2.uniforms.fHeat.value = player.rpm * 2.25 + 0.25;
-	mat2.uniforms.fTime.value = (mat2.uniforms.fTime.value + delta * (player.rpm * 10 + 0.1)) % 1;
+	mat2.uniforms.fTime.value = (mat2.uniforms.fTime.value + delta * (player.rpm * 12 + 0.8)) % 1;
+	mat2.uniforms.mNorm.value = player.mesh.matrixWorld;
 
 	// Set audio speed?
 	if (sounds.length > 0) sounds[0].playbackRate.value = (player.rpm + 0.12) * 2 > 0.35 ? (player.rpm + 0.12) * 2 : 0.35;
